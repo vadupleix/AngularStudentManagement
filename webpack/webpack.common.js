881e50c569398fbee2e73644f2450108ffc0d4e2
vadupleix <vadupleix@foxmail.com>
2020-01-11 01:27:27 -0800

    update online add

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -55,7 +55,7 @@ module.exports = (options) => ({
                 // If this URL is left empty (""), then it will be relative to the current context.
                 // If you use an API server, in `prod` mode, you will need to enable CORS
                 // (see the `jhipster.cors` common JHipster property in the `application-*.yml` configurations)
-                SERVER_API_URL: `'http://localhost:8080/'`
+                SERVER_API_URL: `'http://full-stack-LB-1465993327.us-east-2.elb.amazonaws.com:8080/'`
             }
         }),
         new CopyWebpackPlugin([
